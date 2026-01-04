2714d429a068fdd80d6a751f298fea01b69d16f4
Timo Eberl <timo@timoeberl.de>
2026-01-05 00:53:45 +0100

    Update blick demo

--- demos/blick_demo/main.c
+++ demos/blick_demo/main.c
@@ -6,37 +6,14 @@
 // Doesn't use tics. Showcases the functionality of Blick, the debug viewer.
 
 int main() {
-	// 1. Initialize (Forks the viewer process)
+	// Initialize (Forks the viewer process)
 	blick_init("blick_viewer");
 
 	printf("[HOST] Simulation starting...\n");
 	blick_vec3 pos = {0, 20, 0};
 
-	// 2. Simulation Loop
-	for (int i = 0; i < 10; i++) {
-		blick_start_frame();
-
-		pos.y -= 0.5f;
-
-		blick_record_point(pos, 0.05f, 0xFF00FF00, true);  // light green trail (permanent)
-		blick_record_point(pos, 0.2f, 0xFF003300, false); // dark green
-
-		blick_update_frame();
-
-		usleep(100000);
-
-		blick_vec3 vel_end = {pos.x, pos.y - 1.0f, pos.z};
-		blick_record_line(pos, vel_end, 0xFFFF0000, false); // Red
-
-		blick_end_frame();
-
-		usleep(100000);
-	}
-	blick_shutdown();
-	blick_init("blick_viewer");
-
-	// 2. Simulation Loop
-	for (int i = 0; i < 10; i++) {
+	// Simulation Loop
+	for (int i = 0; i < 50; i++) {
 		blick_start_frame();
 
 		pos.y -= 0.5f;
