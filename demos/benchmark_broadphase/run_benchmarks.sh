67800407bbc54a124c45bbfce44621b9e9ea95b6
Timo Eberl <timo@timoeberl.de>
2026-03-21 14:59:42 +0100

    adjust benchmark 1 number of particles

--- demos/benchmark_broadphase/run_benchmarks.sh
+++ demos/benchmark_broadphase/run_benchmarks.sh
@@ -5,7 +5,7 @@ set -e
 mkdir -p results
 
 echo "=== Running Benchmark 1 (Scaling Particles) ==="
-for PARTICLES in 2500 5000 7500 10000 12500 15000 17500 20000; do
+for PARTICLES in 2500 5000 7500 10000 12500 15000 17500 20000 22500 25000 27500 30000 32500 35000 37500 40000; do
     BIN_PATH="./bench_bin/bench1_particles_${PARTICLES}"
     OUT_FILE="results/bench1_particles_${PARTICLES}.txt"
 
