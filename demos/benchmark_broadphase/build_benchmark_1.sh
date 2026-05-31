3f124c1522b4a0d64c806c98abaff9de0bb611be
Timo Eberl <timo@timoeberl.de>
2026-05-31 21:18:33 +0200

    Update benchmark build scripts to fit new scenario

--- demos/benchmark_broadphase/build_benchmark_1.sh
+++ demos/benchmark_broadphase/build_benchmark_1.sh
@@ -6,8 +6,17 @@ mkdir -p bench_bin
 
 echo "=== Building Benchmark 1 (Scaling Particles) ==="
 STEPS_1=2000
-RES_1=100
 CELL_SIZE_1="1.0"
+
+# Required minimum dimensions: 65x65x115
+RES_X=65
+RES_Y=65
+RES_Z=115
+
+# Origin string expressions evaluated by the C compiler
+ORG_X="(${RES_X}.0 * ${CELL_SIZE_1} * -0.5f)"
+ORG_Y="(${RES_Y}.0 * ${CELL_SIZE_1} * -0.5f)"
+ORG_Z="(${RES_Z}.0 * ${CELL_SIZE_1} * -0.5f)"
 
 for PARTICLES in 2500 5000 7500 10000 12500 15000 17500 20000 22500 25000 27500 30000 32500 35000 37500 40000; do
     echo "Configuring and building Benchmark 1 with $PARTICLES particles..."
@@ -15,10 +24,11 @@ for PARTICLES in 2500 5000 7500 10000 12500 15000 17500 20000 22500 25000 27500 
     cmake -S . -B build_release/ -DCMAKE_BUILD_TYPE=Release -DTICS_ENABLE_CUDA=ON -DTICS_ENABLE_DEBUG_VIEW=OFF -DCMAKE_C_COMPILER=gcc \
         -DBENCHMARK_STEPS=$STEPS_1 \
         -DBENCHMARK_PARTICLE_COUNT=$PARTICLES \
-        -DGRID_RES_X=$RES_1 -DGRID_RES_Y=$RES_1 -DGRID_RES_Z=$RES_1 \
-        -DGRID_CELL_SIZE=$CELL_SIZE_1
+        -DGRID_RES_X=$RES_X -DGRID_RES_Y=$RES_Y -DGRID_RES_Z=$RES_Z \
+        -DGRID_CELL_SIZE=$CELL_SIZE_1 \
+        -DGRID_ORIGIN_X="$ORG_X" -DGRID_ORIGIN_Y="$ORG_Y" -DGRID_ORIGIN_Z="$ORG_Z"
 
-    cmake --build build_release/ --config Release
+    cmake --build build_release/ --config Release --parallel
     
     # Copy and rename the binary so it isn't overwritten
     cp build_release/bin/benchmark_broadphase bench_bin/bench1_particles_${PARTICLES}
--- demos/benchmark_broadphase/build_benchmark_2.sh
+++ demos/benchmark_broadphase/build_benchmark_2.sh
@@ -11,24 +11,27 @@ PARTICLES_2=100000
 for CELL in 1 5 10 15 20; do
     for STRATEGY in A B_NAIVE B_HALF_SHELL; do
         
-        # Determine Grid Resolution to cover at least 100m
-        if [ "$CELL" -eq 1 ]; then RES=100
-        elif [ "$CELL" -eq 5 ]; then RES=20
-        elif [ "$CELL" -eq 10 ]; then RES=10
-        elif [ "$CELL" -eq 15 ]; then RES=7   # 7 * 15 = 105m
-        elif [ "$CELL" -eq 20 ]; then RES=5
-        fi
+        # Determine Grid Resolution to cover at least 65x65x115m using ceiling math: (A + B - 1) / B
+        RES_X=$(( (65 + CELL - 1) / CELL ))
+        RES_Y=$(( (65 + CELL - 1) / CELL ))
+        RES_Z=$(( (115 + CELL - 1) / CELL ))
 
-        echo "Configuring Strategy: $STRATEGY | Cell Size: $CELL.0 | Resolution: $RES..."
+        # Origin string expressions evaluated by the C compiler
+        ORG_X="(${RES_X}.0 * ${CELL}.0 * -0.5f)"
+        ORG_Y="(${RES_Y}.0 * ${CELL}.0 * -0.5f)"
+        ORG_Z="(${RES_Z}.0 * ${CELL}.0 * -0.5f)"
+
+        echo "Configuring Strategy: $STRATEGY | Cell Size: $CELL.0 | Resolution: ${RES_X}x${RES_Y}x${RES_Z}..."
 
         cmake -S . -B build_release/ -DCMAKE_BUILD_TYPE=Release -DTICS_ENABLE_CUDA=ON -DTICS_ENABLE_DEBUG_VIEW=OFF -DCMAKE_C_COMPILER=gcc \
             -DBENCHMARK_STEPS=$STEPS_2 \
             -DBENCHMARK_PARTICLE_COUNT=$PARTICLES_2 \
-            -DGRID_RES_X=$RES -DGRID_RES_Y=$RES -DGRID_RES_Z=$RES \
+            -DGRID_RES_X=$RES_X -DGRID_RES_Y=$RES_Y -DGRID_RES_Z=$RES_Z \
             -DGRID_CELL_SIZE=${CELL}.0 \
+            -DGRID_ORIGIN_X="$ORG_X" -DGRID_ORIGIN_Y="$ORG_Y" -DGRID_ORIGIN_Z="$ORG_Z" \
             -DGPU_BROADPHASE_STRATEGY=$STRATEGY
 
-        cmake --build build_release/ --config Release
+        cmake --build build_release/ --config Release --parallel
         
         # Copy and rename the binary with cell size in the file name
         cp build_release/bin/benchmark_broadphase bench_bin/bench2_cell_${CELL}_${STRATEGY}
